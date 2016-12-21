989a57b731d4cd7c08f407b2a92250132f255f3c
AhiShahar <ahishahar1987@gmail.com>
2016-12-21 15:31:06 +1100

    basic animal database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# t.string :name
+# t.text :description
+# t.string :vidURL
+# t.string :URL
+# t.integer :totalDonation
+# t.string :threatLevel
+Animal.destroy_all();
+
+a1 = Animal.create([{name: "Rhino"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a2 = Animal.create([{name: "Giraffe"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a3 = Animal.create([{name: "Lion"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a4 = Animal.create([{name: "Hippopotamus"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a5 = Animal.create([{name: "Elephant"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a6 = Animal.create([{name: "Tiger"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a7 = Animal.create([{name: "Gorilla"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a8 = Animal.create([{name: "Sloth"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a9 = Animal.create([{name: "Cheetah"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a10 = Animal.create([{name: "Orangutan"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a11 = Animal.create([{name: "Komodo Dragon"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a12 = Animal.create([{name: "Tapir"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a13 = Animal.create([{name: "Red Panda"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a14 = Animal.create([{name: "Panda"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a15 = Animal.create([{name: "Snow Leopard"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a16 = Animal.create([{name: "Emperor Penguin"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a17 = Animal.create([{name: "Polar Bear"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a18 = Animal.create([{name: "Narwhal"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a19 = Animal.create([{name: "Orca"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a20 = Animal.create([{name: "Blue Whale"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a21 = Animal.create([{name: "Great White Shark"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a22 = Animal.create([{name: "Galapagos Tortoise"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a23 = Animal.create([{name: "Dolphin"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a24 = Animal.create([{name: "Sea Lion"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
+a25 = Animal.create([{name: "Dugong"},{url: "#"},{vidURL: "https://www.youtube.com/embed/EHB_CM86rgk"},{threatLevel: "0"},{description: ""}])
